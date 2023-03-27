0b2a130b67e9265533abb791b3a8a9506a5ea2ac
sakshi <sakshi@sakshinoMacBook-Air.local>
2023-03-27 11:53:50 +0530

    Updates

---
+++ precache-manifest.4d7456fb9440825508b5e0e8b0fc875e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f723346e50b2c87489870d1cfe82e64",
+    "url": "/keeperProject/index.html"
+  },
+  {
+    "revision": "7511c847d8a8055f87a1",
+    "url": "/keeperProject/static/js/2.d1a95661.chunk.js"
+  },
+  {
+    "revision": "8dfd60d2e7f889a100f7",
+    "url": "/keeperProject/static/js/main.5776e85c.chunk.js"
+  },
+  {
+    "revision": "a8766becfbb3904f37c7",
+    "url": "/keeperProject/static/js/runtime-main.442c9ea1.js"
+  }
+]);
